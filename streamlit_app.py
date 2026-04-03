2f731402888b2d212bdbe3340858a3732df2fac4
Beatrice Segbegnon <basegbegnon@gmail.com>
2026-04-03 15:10:11 -0400

    Refactor fruit selection and API call
    Removed unnecessary stop command and updated API call to use selected fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,6 @@ fruit_df = (
 # Convert Snowpark DataFrame to a simple Python list for Streamlit widgets
 fruit_list = [row["FRUIT_NAME"] for row in fruit_df.collect()]
 
-
-st.stop()
-
-
 # User input
 name_on_order = st.text_input("Name on Smoothie:")
 ingredients_list = st.multiselect(
@@ -61,8 +57,12 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
