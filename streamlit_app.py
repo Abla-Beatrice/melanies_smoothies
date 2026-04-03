94fa6c72e65f6253d35f4121f3e8e29db6552fab
Beatrice Segbegnon <basegbegnon@gmail.com>
2026-04-03 14:26:49 -0400

    Add dataframe display for smoothiefroot nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,4 @@ if st.button("Submit Order"):
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
